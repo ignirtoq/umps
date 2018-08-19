0e5e3f4a02c9dc11e9a99bf55dedf256c6af53be
Jeffrey Bouas <ignirtoq@gmail.com>
2018-08-18 20:36:51 -0500

    Expanded README description and improved setup.py.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # umps
-Publish-subscribe protocol based on UDP multicast.
+**UDP Multicast Publish-Subscribe** is a publish-subscribe group
+communication system based on IP multicast.
+It is designed to be an extremely lightweight, scalable messaging system for
+use in private (home or corporate) networks.
+
+### Broker-less Subscription Management
+Instead of using a broker application to manage subscriptions and distribute
+messages (*application layer* multicast), UMPS uses IP multicast to leverage
+existing network hardware for the distribution of messages (*network layer* 
+multicast).
+
+This bypasses the challenges of scale of brokered publish-subscribe systems,
+where the broker application becomes a bottleneck, at the cost of slightly
+less reliable message delivery.
+
+### Messaging Limitations and Loss Detection
+UDP is an inherently unreliable and limited protocol.
+There is no built-in mechanism for subscribers to know if a message has been
+dropped, and routers only support messages up to a certain size before they are
+broken up into smaller chunks.  If any one of the chunks is lost, the entire
+message is dropped, exacerbating the loss problem.
+
+UMPS supports *message framing* and frame retransmission. Publishers break up
+large messages into smaller *frames* that are transmitted individually.
+Subscribers reconstruct the entire message before providing it to the
+application.
+If a subscriber detects that it is missing a frame, it can request that 
+frame be retransmitted specifically to it.
+
+Message caching handles the low-hanging fruit of intermittent, single-frame
+loss, but it can't guarantee every message will reach every subscriber under
+continuous high network load.
+For this reason, UMPS is intended for private or corporate use, not for
+extremely large-scale use over the open internet, where continuous heavy load
+is much more likely.
--- setup.py
+++ setup.py
@@ -11,34 +11,49 @@ except ImportError:
 
 name = 'umps'
 description = 'Publish-subscribe protocol based on UDP multicast'
-with open('umps/_version.py') as ver_file:
+with open('umps/_version.py', encoding='utf-8') as ver_file:
     ver_text = ver_file.read()
 pattern = re.compile(r"^version = ['\"]([^'\"]*)['\"]", re.MULTILINE)
 version = pattern.search(ver_text).group(1)
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 
-frame_libs = []
-if os.name == 'nt':
-    frame_libs.append('Ws2_32')
-ext_suffix = '.pyx' if HAVE_CYTHON else '.c'
-extensions = [
-    Extension("umps._hash", ["umps/_hash"+ext_suffix], optional=True),
-    Extension("umps._frame", ["umps/_frame"+ext_suffix], optional=True,
-              libraries=frame_libs),
-    Extension("umps._pack", ["umps/_pack"+ext_suffix], optional=True,
-              libraries=frame_libs),
-    Extension("umps._parse", ["umps/_parse"+ext_suffix], optional=True,
-              libraries=frame_libs),
-]
 if HAVE_CYTHON:
-    extensions = cythonize(extensions)
+    frame_libs = []
+    if os.name == 'nt':
+        frame_libs.append('Ws2_32')
+    extensions = cythonize([
+        Extension("umps._hash", ["umps/_hash.pyx"], optional=True),
+        Extension("umps._frame", ["umps/_frame.pyx"], optional=True,
+                  libraries=frame_libs),
+        Extension("umps._pack", ["umps/_pack.pyx"], optional=True,
+                  libraries=frame_libs),
+        Extension("umps._parse", ["umps/_parse.pyx"], optional=True,
+                  libraries=frame_libs),
+    ])
+else:
+    extensions = None
 
 
 setup(
     name=name,
     description=description,
+    long_description=long_description,
     version=version,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
     packages=find_packages(),
     ext_modules=extensions,
     zip_safe=False,
+    python_requires='>=3.5'
       )
